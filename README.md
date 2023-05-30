1477252801f0633c89634714886cb2a8b3572e0f
Mike Cotterman <mcotterman@gmail.com>
2023-05-30 15:19:45 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,250 @@
+# Hummingbird bit Library for micro:bit v2
+
+Library to use the [BirdBrain](https://www.birdbraintechnologies.com/) [Hummingbird bit](https://www.birdbraintechnologies.com/products/hummingbird-bit-robotics-kit/) with [MicroPython on the micro:bit v2](https://python.microbit.org/v/3).
+
+**Refactored from the MakeCode library for MicroPython on the micro:bit v2 by CodeJoy (codejoy.org)**
+
+_____
+
+## Important notes:
+* We attempted to be as true as possible to the original MakeCode library during the refactor with regard to method names and arguments, but there are some differences due to syntactical differences between the languages
+   * [Original Birdbrain MakeCode library for Hummingbird bit](https://github.com/BirdBrainTechnologies/pxt-hummingbird-bit) (MIT License)
+* **IMPORTANT**: This library was built and tested using the micro:bit v2. It is unlikely to function properly on the micro:bit v1 due to memory constraints.**
+_____
+## API Reference
+---
+### startHummingbird()
+This initialization method must be run after all your imports at the top of your python script and after your have imported the Hummingbird library.
+```
+    from microbit import *
+    from hummingbird import hummingbird
+    # ... any other needed libraries ...
+
+    hummingbird.startHummingbird()
+
+    # ... the rest of your code ...
+```
+---
+
+### setLED(port, intensity)
+Controls LEDs attached to the LED ports on the Hummingbird.
+
+`hummingbird.setLED(port, intensity)`
+
+This method has 2 arguments:
+* `port`: This is an integer that represent the port number you wish to control (1 - 3)
+* `intensity`: The brightness to set for the LED as a percentage (0 - 100) (Integer) [0 = Off]
+
+```
+    # Sets the LED in LED port 3 to 80% brightness
+    hummingbird.setLED(3, 80)
+```
+
+---
+
+### setTriLED(port, red, green, blue)
+Controls the status and brightness of a Tri-Color (RGB) LED attached to one of the Tri-Color ports. Mixing color brightnesses can produce a variety of colors.
+
+`hummingbird.setTriLED(port, red, green, blue)`
+
+This method has 4 arguments:
+* `port`: This is an integer that represent the port number you wish to control (1 - 2)
+* `red`: The brightness to set for the red color of the LED as a percentage (0 - 100) (Integer) [0 = Off]
+* `green`: The brightness to set for the green color of the LED as a percentage (0 - 100) (Integer) [0 = Off]
+* `blue`: The brightness to set for the blue color of the LED as a percentage (0 - 100) (Integer) [0 = Off]
+
+```
+    # Sets the Tri-color LED in Tri-color port 1 80% red, 0% green and 10% blue
+    hummingbird.setLED(1, 80, 0, 10)
+```
+
+---
+
+### setPositionServo(port, angle)
+Sets the angle of a position servo attached to one of the servo ports on the Hummingbird.
+
+`hummingbird.setPositionServo(port, angle)`
+
+This method has 2 arguments:
+* `port`: This is an integer that represent the port number you wish to control (1 - 4)
+* `angle`: The angle to which to move the servo (0 - 180) (Integer)
+
+```
+    # Sets the position servo in servo port 2 to 75 degrees
+    hummingbird.setPositionServo(2, 75)
+```
+
+---
+
+### setRotationServo(port, speed)
+Sets rotation speed of a rotation servo attached to one of the servo ports on the Hummingbird.
+
+`hummingbird.setRotationServo(port, speed)`
+
+This method has 2 arguments:
+* `port`: This is an integer that represent the port number you wish to control (1 - 4)
+* `speed`: the percentage based speed at which to spin the servo (-100 - 100) (Integer) [0 = Stop]
+    * When looked at from the top of the servo (the part with the horn or connector) the servo should rotate counter clockwise with positive number and clockwise with negative numbers
+    * The max speed of will vary a bit from servo to servo and between positive and negative numbers on the same servo
+
+```
+    # Sets the rotation servo in servo port 3 to rotate at -60% (clockwise)
+    hummingbird.setRotationServo(3, -60)
+```
+
+---
+
+### getSensor(sensor_type, port)
+Returns the value of the sensor attached to one of the sensor ports on the Hummingbird.
+
+`hummingbird.getSensor(sensor_type, port)`
+
+**Sensor list:**
+* Light (hummingbird.sensor_light or 1)
+    * Returns an integer value 0 - 100 based on the intensity of the light (No units)
+* Dial (hummingbird.sensor_dial or 2)
+    * Returns an Returns an integer value 0 - 100 based on where the dial (potentiometer) is in it's available rotation range (No units)
+* Distance (hummingbird.sensor_distance or 3)
+    * Returns the approximate distance from the sensor in centimeters (cm)
+* Sound (hummingbird.sensor_sound or 4)
+    * Returns an integer value 0 - 100 based on the intensity/volume of the sound detected (No units)
+* Other (hummingbird.sensor_other or 5):
+    * This one is very dependant on what you attach, but will generally return an integer value 0 - 100 based on the intensity of whatever it is detecting (No units)
+
+This method has 2 arguments:
+* `sensor`: the type of sensor attached to the port (Integer)
+    * `hummingbird.sensor_light` or `1`
+    * `hummingbird.sensor_dial` or `2`
+    * `hummingbird.sensor_distance` or `3`
+    * `hummingbird.sensor_sound` or `4`
+    * `hummingbird.sensor_other` or `5`
+* `port`: This is an integer that represent the port number you wish to control (1 - 3)
+
+```
+    # Get the current value from the Distance sensor attached to sensor port 2
+    hummingbird.getSensor(hummingbird.sensor_distance, 2)
+```
+
+---
+
+### getBattery()
+Returns the voltage of the battery in millivolts. 
+
+___Note: The device my start experiencing difficulty at a reading of about 4630 mV.~___
+
+`hummingbird.getBattery()`
+
+```
+    # Get the current voltage from the battery in milliVolts
+    hummingbird.getBattery()
+```
+_____
+
+## Sample Code:
+
+Below are some basic sample code projects to test out python on your micro:bit v2
+
+Notes:
+* Before running this code you must add the hummingbird.py file to your python project.
+
+---
+
+### Code 1: Controlling rotation servos with microi:bit buttons
+
+What the code does:
+* When the program starts the rotation servos in servo ports 3 and 4 are stopped
+* When button A is pressed the rotation servos in servo ports 3 and 4 are set to -50% and the `-` character is displayed on the 5x5 LED matrix.
+* When button B is pressed the rotation servos in servo ports 3 and 4 are set to 50% and the `+` character is displayed on the 5x5 LED matrix.
+* When both button and and B have been pressed the rotation servos in servo ports 3 and 4 are stopped and the letter "S" is displayed on the 5x5 LED matrix.
+* The program sleeps (pauses) for 100 milliseconds at the end of each loop
+
+```
+from microbit import *
+from hummingbird import hummingbird
+
+hummingbird.startHummingbird()
+
+rSpeed = 0
+
+while True:
+    hummingbird.setRotationServo(3, rSpeed)
+    hummingbird.setRotationServo(4, rSpeed)
+    ba = button_a.was_pressed()
+    bb = button_b.was_pressed()
+    if ba and bb:
+        display.show("S")
+        rSpeed = 0    
+    elif ba:
+        display.show("-")
+        rSpeed = -50
+    elif bb:
+        display.show("+")
+        rSpeed = 50
+    sleep(100)
+```
+
+What the code does:
+* Displays the value of the distance and dial sensors on the serial console
+* The distance sensor in sensor port 1 is used to control the color of the tri-color LED in tricolor port 1
+  * The LED is green if the distance sensor reads further than or equal to 40cm
+  * The LED is yellow if the distance sensor reads closer than 40cm, but further than 20 cm
+  * The LED is red if the distance sensor reads further than or equal to 20cm
+* The dial sensor in sensor port 2 is used to control the angle of the position servo attached to servo port 1
+
+
+```
+    from microbit import *
+    from hummingbird import hummingbird
+
+    hummingbird.startHummingbird()
+
+    last_angle = -1
+    dial = 0
+    distance = 0
+
+    while True:
+        distance = hummingbird.getSensor(hummingbird.sensor_distance, 1)
+        dial = hummingbird.getSensor(hummingbird.sensor_dial, 2)
+
+        print("Distance: {}cm".format(distance))
+        print("Dial: {}".format(dial))
+
+        # Control Tri-color LED in tri-color port 1 based on distance sensor
+        if distance >= 40:
+            hummingbird.setTriLED(1, 0, 100, 0)
+        elif distance < 40 and distance > 20:
+            hummingbird.setTriLED(1, 100, 80, 0)
+        elif distance <= 20:
+            hummingbird.setTriLED(1, 100, 0, 0)
+
+        # Control the postion servo in servo port 1 based on the value of the dial sensor
+        # The angle formula is simplified from dial * 180 / 100
+        angle = round(dial * 1.8)
+        if last_angle != angle:
+            hummingbird.setPositionServo(1, angle)
+            last_angle = angle
+
+        sleep(100)
+```
+
+_____
+
+## License
+
+Copyright 2023 CodeJoy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy ofthis software and associated documentation files (the “Software”), to deal in the
+Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ hummingbird.py
@@ -0,0 +1,345 @@
+"""
+Library to use the BirdBrain Hummingbird bit with MicroPython on the micro:bit v2.
+
+Refactored from the MakeCode library for MicroPython on the micro:bit v2 by CodeJoy (codejoy.org)
+Original MakeCode library: https://github.com/BirdBrainTechnologies/pxt-hummingbird-bit (MIT License)
+
+Note: I tried to be as true as possible to the original MakeCode library during the refactor.
+
+IMPORTANT: This library was built and tested using the micro:bit v2. It is unlikely to 
+            function properly on the micro:bit v1 due to memory constraints.
+
+Copyright 2023 CodeJoy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in the
+Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from microbit import *
+from time import sleep_us
+
+class hummingbird:
+    readyToSend = False
+    waitTime_1 = 4 # microseconds
+    waitTime_2 = 100 # microseconds
+    waitTime_Initial = 500 # microseconds
+    waitTime_Start = 2000
+
+    # Sensor ID List
+    sensor_light = 1
+    sensor_dial = 2
+    sensor_distance = 3
+    sensor_sound = 4
+    sensor_other = 5
+
+    @staticmethod
+    def startHummingbird():
+        """Must be run before any other Hummingbird commands. This initializes the Hummingbird bit.
+        """
+
+        # ********** Initialize and setup Hummingbird bit
+        pin0.write_analog(0)
+        sleep(hummingbird.waitTime_Start)
+        pin16.write_digital(1)
+        spi.init(baudrate=1000000, bits=8, mode=0, sclk=pin13, mosi=pin15, miso=pin14)
+        sleep_us(hummingbird.waitTime_Initial)
+        pin16.write_digital(0)
+        sleep_us(hummingbird.waitTime_1)
+        spi.write(b'\xCB')
+        sleep_us(hummingbird.waitTime_2)
+        spi.write(b'\xFF')
+        sleep_us(hummingbird.waitTime_2)
+        spi.write(b'\xFF')
+        sleep_us(hummingbird.waitTime_2)
+        spi.write(b'\xFF')
+        sleep_us(hummingbird.waitTime_1)
+        pin16.write_digital(1)
+        hummingbird.readyToSend = True
+        
+        # Set LEDs 2 and 3 to 0
+        pin2.write_analog(0)
+        pin8.write_analog(0)
+    
+        print('Hummingbird READY')
+        display.show(Image.FABULOUS)
+        
+    @staticmethod
+    def setLED(port: int, intensity: int):
+        """Controls the status and brightness of an LED attached to one of the LED ports
+
+        :param port: the port number (1-3) to control (Integer)
+        :param intensity: the brightness as a percentage (0 - 100) (Integer) [0 = Off]
+        """
+ 
+        # LED Control
+        timeout = 0
+        while (not hummingbird.readyToSend and timeout < 25):
+            sleep(10)
+            timeout += 1
+            
+        if (port >= 1 and port <= 3 and hummingbird.readyToSend):
+            intensityScaled = 0
+            if (intensity > 100):
+                intensity = 100
+            if (intensity < 0):
+                intensity = 0
+            intensityScaled = int((intensity * 255) / 100)
+        
+            while (not hummingbird.readyToSend): # Wait for other functions in other threads
+                sleep_us(10)
+
+            hummingbird.readyToSend = False
+            if (port == 1):
+                display.scroll(1)
+                sleep_us(hummingbird.waitTime_Initial)
+                pin16.write_digital(0)
+                sleep_us(hummingbird.waitTime_1)
+                spi.write(b'\xC0')
+                sleep_us(hummingbird.waitTime_2)
+                spi.write(bytes([intensityScaled]))
+                sleep_us(hummingbird.waitTime_2)
+                spi.write(b'\xFF')
+                sleep_us(hummingbird.waitTime_2)
+                spi.write(b'\xFF')
+                sleep_us(hummingbird.waitTime_1)
+                pin16.write_digital(1)
+            elif (port == 2):
+                display.scroll(2)
+                pin2.write_analog(intensityScaled * 4)
+            elif (port == 3):
+                display.scroll(3)
+                pin8.write_analog(intensityScaled * 4)
+            
+            hummingbird.readyToSend = True
+    
+    @staticmethod
+    def setTriLED(port, red = 50, green = 0, blue = 50):
+        """Controls the status and brightness of a Tri-Color (RGB) LED attached to one of the Tri-Color ports. 
+            Mixing color brightnesses can produce a variety of colors.
+
+        :param port: the port number (1-2) to control (Integer)
+        :param red: the brightness as a percentage (0 - 100) for the color red (Integer) [0 = Off]
+        :param green: the brightness as a percentage (0 - 100) for the color green (Integer) [0 = Off]
+        :param blue: the brightness as a percentage (0 - 100) for the color blue (Integer) [0 = Off]
+        """
+
+        timeout = 0
+        while (not hummingbird.readyToSend and timeout < 25):
+            sleep(10)
+            timeout += 1
+            
+        if (port >= 1 and port <= 2 and hummingbird.readyToSend):
+            red = max(0, min(red, 100))
+            green = max(0, min(green, 100))
+            blue = max(0, min(blue, 100))
+            port_val = 0xC3 + port
+            red = int(red * 255 / 100)
+            green = int(green * 255 / 100)
+            blue = int(blue * 255 / 100)
+    
+            while (not hummingbird.readyToSend):
+                sleep_us(10)
+            hummingbird.readyToSend = False
+            sleep_us(hummingbird.waitTime_Initial)
+            pin16.write_digital(0)
+            sleep_us(hummingbird.waitTime_1)
+            spi.write(bytes([port_val]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([red]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([green]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([blue]))
+            sleep_us(hummingbird.waitTime_1)
+            pin16.write_digital(1)
+            hummingbird.readyToSend = True
+    
+    @staticmethod
+    def setPositionServo(port:int, angle:int = 90):
+        """Sets the angle of a position servo attached to one of the servo ports.
+
+        :param port: the port number (1-3) to control (Integer)
+        :param angle: the angle to which to move the servo (0 - 180) (Integer)
+        """
+        timeout = 0
+        while (not hummingbird.readyToSend and timeout < 25):
+            sleep(10)
+            timeout += 1
+        if (port >= 1 and port <= 4 and hummingbird.readyToSend):
+            angle_conv = 0
+            angle = max(0, min(angle, 180))
+    
+            angle_conv = int((angle * 254) / 180)
+            port_val = 0xC5 + port
+            # print("servo {} ({}) - {}".format(port, port_val, angle))
+            while (not hummingbird.readyToSend):
+                sleep_us(10)
+            hummingbird.readyToSend = False
+            sleep_us(hummingbird.waitTime_Initial)
+            pin16.write_digital(0)
+            sleep_us(hummingbird.waitTime_1)
+            spi.write(bytes([port_val]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([angle_conv]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([0xFF]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([0xFF]))
+            sleep_us(hummingbird.waitTime_1)
+            pin16.write_digital(1)
+            hummingbird.readyToSend = True
+    
+    @staticmethod
+    def setRotationServo(port:int, speed:int = 0):
+        """Sets rotation speed of a rotation servo attached to one of the servo ports.
+
+        :param port: the port number (1-3) to control (Integer)
+        :param speed: the percentage based speed at which to spin the servo (-100 - 100) (Integer) [0 = Stop]
+        """
+
+        timeout = 0
+        while (not hummingbird.readyToSend and timeout < 25):
+            sleep(10)
+            timeout += 1
+        
+        if (port >= 1 and port <= 4 and hummingbird.readyToSend):
+            speed_conv = 0
+            speed = max(-100, min(speed, 100))
+            
+            # Send the off command if the speed is close to zero. Otherwise,
+            # convert the speed to a range from 104 to 144 (found experimentally to
+            # be the command range for the rotation servo)
+            if (speed > -10 and speed < 10):
+                speed_conv = 255
+            else:
+                speed_conv = int((speed * 23 / 100) + 122)
+    
+            port_val = 0XC5 + port
+    
+            while (not hummingbird.readyToSend):
+                sleep_us(10)
+            hummingbird.readyToSend = False
+            sleep_us(hummingbird.waitTime_Initial)
+            pin16.write_digital(0)
+            sleep_us(hummingbird.waitTime_1)
+            spi.write(bytes([port_val]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([speed_conv]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([0xFF]))
+            sleep_us(hummingbird.waitTime_2)
+            spi.write(bytes([0xFF]))
+            sleep_us(hummingbird.waitTime_1)
+            pin16.write_digital(1)
+            hummingbird.readyToSend = True
+    
+    @staticmethod
+    def getSensor(sensor:int, port:int):
+        """Returns the value of the sensor attached to one of the sensor ports
+            Sensor list:
+                Light (hummingbird.sensor_light or 1):
+                    Returns an integer value 0 - 100 based on the intensity of the light (No units)
+                Dial (hummingbird.sensor_dial or 2):
+                    Returns an Returns an integer value 0 - 100 based on where the dial (potentiometer) is in it's available rotation range (No units)
+                Distance (hummingbird.sensor_distance or 3):
+                    Returns the approximate distance from the sensor in centimeters (cm)
+                Sound (hummingbird.sensor_sound or 4):
+                    Returns an integer value 0 - 100 based on the intensity/volume of the sound detected (No units)
+                Other (hummingbird.sensor_other or 5):
+                    This one is very dependant on what you attach, but will generally return an integer value 0 - 100 based on the intensity of whatever it is detecting (No units)
+        
+        :param sensor: the type of sensor attached to the port (Integer) \
+            [ hummingbird.sensor_light | \
+            hummingbird.sensor_dial | \
+            hummingbird.sensor_distance | \
+            hummingbird.sensor_sound | \
+            hummingbird.sensor_other ]
+            
+        :param port: the port number (1-3) to control (Integer)
+        """
+ 
+        timeout = 0
+        while (not hummingbird.readyToSend and timeout < 25):
+            sleep(10)
+            timeout += 1
+        
+        if (port >= 1 and port <= 3 and hummingbird.readyToSend):
+            vals = [bytearray(1), bytearray(1), bytearray(1), bytearray(1)]
+            while (not hummingbird.readyToSend):
+                sleep_us(10)
+            hummingbird.readyToSend = False
+            # Need to read all three sensor values and the battery (vals[3]) to complete the communication protocol.
+            sleep_us(hummingbird.waitTime_Initial)
+            pin16.write_digital(0)
+            sleep_us(hummingbird.waitTime_1)
+            spi.write_readinto(bytearray(b'\xCC'), vals[0])
+            sleep_us(hummingbird.waitTime_2)
+            spi.write_readinto(bytearray(b'\x66'), vals[1])
+            sleep_us(hummingbird.waitTime_2)
+            spi.write_readinto(bytearray(b'\x77'), vals[2])
+            sleep_us(hummingbird.waitTime_2)
+            spi.write_readinto(bytearray(b'\x88'), vals[3])
+            sleep_us(hummingbird.waitTime_1)
+            spi.write_readinto(bytearray(b'\x55'), vals[0])
+            sleep_us(hummingbird.waitTime_1)
+            spi.write_readinto(bytearray(b'\x66'), vals[1])
+            sleep_us(hummingbird.waitTime_1)
+            pin16.write_digital(1)
+            hummingbird.readyToSend = True
+            if sensor == 3: # Distance
+                return round(int.from_bytes(vals[port - 1], 'big') * 117 / 100)
+            elif sensor == 4: # Sound
+                return round(int.from_bytes(vals[port - 1],'big') * 200 / 255)
+            elif sensor == 2: # Dial
+                return min(round(int.from_bytes(vals[port - 1], 'big') * 100 / 230), 100)
+            else:
+                return round((int.from_bytes(vals[port - 1], 'big') * 100) / 255)
+                
+        return 0
+    
+    @staticmethod
+    def getBattery():
+        """Returns the voltage of the battery in millivolts. The device my start experiencing difficulty at a reading of about 4630 mV.
+        """
+        timeout = 0
+        while (not hummingbird.readyToSend and timeout < 25):
+            sleep(10)
+            timeout += 1
+        if hummingbird.readyToSend:
+            while (not hummingbird.readyToSend):
+                sleep_us(10)
+            hummingbird.readyToSend = False
+    
+            batt = bytearray(1)
+            junk = bytearray(1)
+            sleep_us(hummingbird.waitTime_Initial)
+            pin16.write_digital(0)
+            sleep_us(hummingbird.waitTime_1)
+            # spi.write(bytearray(b'\x55'))
+            spi.write_readinto(bytearray(b'\x55'), junk)
+            sleep_us(hummingbird.waitTime_2)
+            spi.write_readinto(bytearray(b'\x66'), junk)
+            sleep_us(hummingbird.waitTime_2)
+            spi.write_readinto(bytearray(b'\x77'), junk)
+            sleep_us(hummingbird.waitTime_2)
+            spi.write_readinto(bytearray(b'\x88'), batt)
+            sleep_us(hummingbird.waitTime_1)
+            pin16.write_digital(1)
+            hummingbird.readyToSend = True
+            return round(int.from_bytes(batt, 'big') * 406 / 10)
+                
+        return 0
